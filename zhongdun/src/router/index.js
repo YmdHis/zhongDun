c0752052a317f1ffca3c0408cbbb5c53a413fd11
lovejokes <34080719+lovejokes@users.noreply.github.com>
2019-01-10 09:10:15 +0800

    Revert "1"
    This reverts commit 35686df85d606b14aa92461d65b3907b77759cd3.

--- zhongdun/src/router/index.js
+++ zhongdun/src/router/index.js
@@ -1,22 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '@/pages/home/home'
-import step1 from '@/pages/works/step1'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/home',
-      name: 'home',
-      component: home
-    },
-    {
-      path: '/works',
-      name: 'step1',
-      component: step1
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
-
   ]
 })
