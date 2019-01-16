60475b3c6354ecb4c5a8236cd1609f1a6914a7c4
zstart <845907306@qq.com>
2019-01-16 15:28:19 +0800

    Update cityPicker.vue

--- zhongdun/src/components/cityPicker.vue
+++ zhongdun/src/components/cityPicker.vue
@@ -4,21 +4,34 @@
             <img src="../images/close.png" class="img-close" @click="closeBtn()"/>
             <p class="header-title">选择地址</p>
         </div>
-        <div>
+        <div class="city_input_box">
+            <div class="city_input_style">
+                <i class="city_input_icon"><img src="../images/sear_icon.png" alt=""></i>
+                <input type="search" name="city" placeholder="请输入学校/商务楼/写字楼等" required="required" class="city_input">
+            </div>
+        </div>
+        <div class="city_positioning">
             当前定位城市：{{guessCity}}  重新定位
         </div>
-        <section>
+        <section class="city_name_box">
             <ul>
                 <li v-for="(value, key, index) in sortgroupcity" :key="key">
-                    <h4>{{key}}
-                        <span v-if="index == 0">（按字母排序）</span>
+                    <h4 :id="key">{{key}}
+                        <span v-if="index == 0">（按首字母排序）</span>
                     </h4>
                     <ul class="clear">
-                        <li v-for="item in value" :key="item.id" class="ellipsis" >{{item.name}}</li>
+                        <li v-for="item in value" :key="item.id" class="ellipsis city_name_li" >{{item.name}}</li>
                     </ul>
                 </li>
             </ul>
         </section>
+        <aside class="letter-aside">
+          <ul>
+            <li v-for="(value, key, index) in sortgroupcity" @click="naver(key)" ref="key">
+                {{key}}
+            </li>
+          </ul>
+        </aside>
     </div>
 </template>
 
@@ -55,17 +68,30 @@ export default {
         //点击图标关闭
         closeBtn(){
             this.$emit("closeMsg", "close");
-        }
-    },
+        },
+         naver: function (id) { // 点击右边字母滚动
+                let obj = document.getElementById(id);
+                 let obj1 = document.getElementById('city_picker');
+                console.log(obj);
+                let oPos = obj.offsetTop;
+                console.log(oPos)
+                return window.scrollTo(0, oPos - 36)
+            }
+    }
+     
 }
 
 </script>
 <style scoped>
 #city_picker{
-    background: #f5f5f5;
+    background: #fff;
 }
 .header{
     width: 100%;
+    background: #fff;
+    position: fixed;
+    top: 0;
+    z-index: 999;
     padding: .3rem .2rem .7rem;
 }
 .img-close{
@@ -78,5 +104,53 @@ export default {
     font-size: .8rem;
     text-align: center;
 }
+.city_input_box{
+    padding:.3rem .5rem;
+    padding-top:2rem; 
+    font-size: .7rem;
+    width: 100%;
+    position: relative;
+}
+.city_input_style{
+    display: block;
+    background: #f5f5f5;
+    width: 100%;
+    padding:.3rem .5rem;
+}
+.city_submit{
+   position: absolute;
+   right: 0;
+}
+.city_input_icon img{
+    width: .7rem;
+    vertical-align: middle;
+}
+.city_input{
+    width: 12rem;
+    background:none;
+}
+.city_name_box h4{
+    background: #f1f1f1;
+    font-size: .5rem;
+    padding: .5rem;
+  }
+  .city_name_li{
+    padding: .5rem;
+     font-size: .7rem;
+  }
+  .letter-aside{
+    position: fixed;
+    right: 0;
+    top: 5rem;
+    width: 2rem;
+    text-align: center;
+    line-height: 1rem;
+    font-size: .5rem;
+
+  }
+  .city_positioning{
+    font-size: .6rem;
+    padding:.5rem .3rem;
+  }
 </style>
 
